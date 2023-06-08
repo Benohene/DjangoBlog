363b6c9c14ed92341e2138ac1c1bb2d0a0f20368
Benjamin Ohene Konadu <120409685+Benohene@users.noreply.github.com>
2023-06-08 17:42:26 +0200

    deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["mb2test.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["blog-ben-2023.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
